72471914d2a1850a106cabcbcae4dfb74b940f96
Coda <o4oban@gmail.com>
2024-03-05 21:49:46 +0800

    Enable Web Analytics in Vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
-import Footer from "#/app/_components/footer";
-import { CMS_NAME, HOME_OG_IMAGE_URL } from "#/lib/constants";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import Footer from '#/app/_components/footer'
+import { CMS_NAME, HOME_OG_IMAGE_URL } from '#/lib/constants'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react'
 
-import "./globals.css";
+import './globals.css'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: `Next.js Blog Example with ${CMS_NAME}`,
@@ -13,12 +14,12 @@ export const metadata: Metadata = {
   openGraph: {
     images: [HOME_OG_IMAGE_URL],
   },
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
@@ -58,7 +59,8 @@ export default function RootLayout({
       <body className={inter.className}>
         <div className="min-h-screen">{children}</div>
         <Footer />
+        <Analytics />
       </body>
     </html>
-  );
+  )
 }
